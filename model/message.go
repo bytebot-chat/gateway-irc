ed38262b077691ba0a9062e3a31383f07b270155
Bren Briggs <code@fraq.io>
2021-05-14 17:27:43 -0400

    Add Marshal and Unmarshal methods to Message

--- model/message.go
+++ model/message.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"encoding/json"
+
 	"github.com/satori/go.uuid"
 	hbot "github.com/whyrusleeping/hellabot"
 )
@@ -14,4 +16,15 @@ type Metadata struct {
 	Source string
 	Dest   string
 	ID     uuid.UUID
+}
+
+func (m *Message) Marshal() ([]byte, error) {
+	return json.Marshal(m)
+}
+
+func (m *Message) Unmarshal(b []byte) error {
+	if err := json.Unmarshal(b, m); err != nil {
+		return err
+	}
+	return nil
 }
